692190397621a46fc5a62bc0c72540056b8ea5bf
Nikema Prophet <prophen@users.noreply.github.com>
2016-09-15 01:19:20 -0700

    Converted the project to Xcode 8/Swift 3

---
+++ rejection-collector/Rejection.swift
@@ -0,0 +1,13 @@
+//
+//  Rejection.swift
+//  rejection-collector
+//
+//  Created by Nikema Prophet on 9/15/16.
+//  Copyright © 2016 Nikema Prophet. All rights reserved.
+//
+
+import Foundation
+
+class Rejection: NSObject {
+    
+}
--- rejection-collector/ViewController.swift
+++ rejection-collector/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
+    @IBOutlet weak var rejectionsLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitButtonClicked(_ sender: AnyObject) {
+        
+    }
 
 }
 
